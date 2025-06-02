0836b4c7cfcb584fb1d536171aa208e830f0dfb5
Emanorion <nextgengamer.prime@gmail.com>
2025-06-02 12:40:36 +0200

    created exersise2

---
+++ exersise2/operations-type.js
@@ -0,0 +1,14 @@
+let number1 = 6;
+let number2 = 9;
+
+stringnumber1 = String(number1)
+console.log(typeof stringnumber1);
+
+sum = number1 + number2
+product = number1 * number2
+
+console.log(`Number 1 is ${number1}, Number 2 is ${number1}. Number 1 is now a ${typeof stringnumber1} the sum of the numbers is ${sum} and the product is ${product}`);
+
+
+
+
