769197e3e1d3e12eb8c2c51582e1278007e59fc9
Emanorion <nextgengamer.prime@gmail.com>
2025-06-02 12:42:21 +0200

    created exersise3

---
+++ exersise3/arrays.js
@@ -0,0 +1,14 @@
+colors = [];
+colors.push("Red","Purple","Grey");
+console.log(colors.join());
+
+
+numbers = [1,2,3,4,5];
+colors.pop();
+colors.unshift("Black")
+console.log(numbers.join());
+console.log(colors.join());
+
+console.log(`The leghth of the array colors is ${colors.length} and numbers is ${numbers.length}`);
+
+
