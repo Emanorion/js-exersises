fe39fd13d2513b6c92e4a91bd0fe08047b562b77
Emanorion <nextgengamer.prime@gmail.com>
2025-06-02 12:35:13 +0200

    create exersise1

---
+++ exersise1/basics.js
@@ -0,0 +1,11 @@
+let firstName = "Emmanuel";
+let lastname = "tembo";
+let age = 19;
+
+console.log(`Hi ${firstName} ${lastname} you are ${age} years old`);
+
+age = 25;
+console.log(`Hi ${firstName}! you are now ${age}`);
+
+
+
