9520ba1ca9aef6907b05e7e7849bbe4c7e62ac29
Emanorion <nextgengamer.prime@gmail.com>
2025-06-02 12:48:45 +0200

    exersise6 created

---
+++ exersise6/function.js
@@ -0,0 +1,19 @@
+// TODO: Create a function called greet that takes a name parameter
+// TODO: Make the greet function return "Hello, [name]!"
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+// TODO: Create a function called add that takes two parameters
+function add(a, b) {
+    return a + b;
+}
+// TODO: Make the add function return the sum of both parameters
+function add(a, b) {
+    return a + b;
+}
+// TODO: Call both functions and store their results in variables
+let greeting = greet("Alice");
+let sum = add(5, 10);
+// TODO: Print the results to the console
+console.log(greeting);
+console.log(sum);
